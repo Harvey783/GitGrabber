011f623d55e59e5a422086a28e107ab14479ca03
Christopher Harvey <chharvey@icloud.com>
2019-03-13 00:01:49 -0400

    constructor ID and secret

--- github.js
+++ github.js
@@ -1,6 +1,6 @@
 class Github {
-  constructor() {}
+  constructor() {
+    this.clientID = "b6844a6263ddd02d0dc5";
+    this.clientSecret = "91c21c9be54a074a91648a52605992c8efefe750";
+  }
 }
-
-// client id b6844a6263ddd02d0dc5
-// secret 91c21c9be54a074a91648a52605992c8efefe750
