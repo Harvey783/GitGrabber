28350fa84529b3edcc17c0fa9feb71838f70c4a2
Christopher Harvey <chharvey@icloud.com>
2019-03-13 00:25:49 -0400

    getUser

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const github = new Github();
+
 const userSearch = document.getElementById("userSearch");
 
 userSearch.addEventListener("keyup", e => {
   const userText = e.target.value;
   if (!userText == "") {
+    github.getUser(userText).then(data => {
+      console.log(data);
+    });
   }
 });
--- github.js
+++ github.js
@@ -3,4 +3,19 @@ class Github {
     this.clientID = "b6844a6263ddd02d0dc5";
     this.clientSecret = "91c21c9be54a074a91648a52605992c8efefe750";
   }
+
+  getUser = async user => {
+    const response = await fetch(`https://api.github.com/users/${user}`, {
+      params: {
+        clientID: this.clientID,
+        clientSecret: this.clientSecret
+      }
+    });
+
+    const profile = await response.json();
+
+    return {
+      profile
+    };
+  };
 }
